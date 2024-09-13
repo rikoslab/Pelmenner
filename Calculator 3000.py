fce66c8d9646ff57cd4edb7462bce57c8f270c1d
rikoslab <75677741+rikoslab@users.noreply.github.com>
2024-09-13 21:19:50 +0300

    Create Calculator 3000.py
    MEGA CALCULATOR

---
+++ Calculator 3000.py
@@ -0,0 +1,28 @@
+print("Welcome to the Calculator 3000")
+
+while True:
+
+    a = float(input('write first number: '))
+    b = float(input('write second number: '))
+    sign = input('write a sign(+, -, /, *): ')
+
+    if "+" in sign:
+        result = a + b
+    elif "-" in sign:
+        result = a - b
+    elif "/" in sign:
+        if b != 0:
+            result = a / b
+            print(int(result))
+        else:
+            print("ERROR")
+            continue
+    elif "*" in sign:
+        result = a * b
+
+    print(int(result))
+
+    print("to Exit write 'exit' or press enter to continue ")
+    exit = input()
+    if exit == "exit":
+        break
