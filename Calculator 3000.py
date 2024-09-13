0a9e0522468a05ab6e7272ddaacc89bb1b22cb90
rikoslab <75677741+rikoslab@users.noreply.github.com>
2024-09-13 23:12:36 +0300

    Update Calculator 3000.py
    решена проблема с повторным выведением ответа после корректного деления

--- Calculator 3000.py
+++ Calculator 3000.py
@@ -2,7 +2,7 @@ print("Welcome to the Calculator 3000")
 print("Написано Низамиевым Тамирланом гр.533")
 
 while True:
-
+    res1 = False ## ФИКС.проверка сделан ли первый результат после деления
     a = float(input('write first number: '))
     b = float(input('write second number: '))
     sign = input('write a sign(+, -, /, *): ')
@@ -14,14 +14,18 @@ while True:
     elif "/" in sign:
         if b != 0:
             result = a / b
-            print(int(result))
+            print(int(result)) ## результат после деления
+            res1 = True ## если произошло деление, повторонрого результата не будет выведено
         else:
-            print("ERROR")
+            print("ERROR") ## команда на проверку деления на 0
             continue
     elif "*" in sign:
         result = a * b
 
-    print(int(result))
+    if res1 == False:
+        print(int(result))
+    else:
+        print("")
 
     print("to Exit write 'exit' or press enter to continue ")
     exit = input()
