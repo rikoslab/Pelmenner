58bf7bb7a543f3992b267a703ef5b2729af1cb86
rikoslab <75677741+rikoslab@users.noreply.github.com>
2024-09-19 19:18:46 +0300

    Add files via upload

---
+++ pyramid.py
@@ -0,0 +1,20 @@
+while True:
+    height = int(input(""))
+    sp = height
+    y = 1
+    while y <= height:
+        while sp > 0:
+            x = 1
+            print(" " * sp, end="")
+            while x < y:
+                print("*", end="")
+                x += 1
+            z = 0
+            while z < y:
+                print("*", end="")
+                z += 1
+            y += 1
+            sp -= 1
+            print("")
+
+    
